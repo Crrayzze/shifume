bcb525593b519f0f0d92f87f87dec811796c9bbb
Killian <paulic.killian@gmail.com>
2023-04-04 22:29:54 +0200

    global style

---
+++ frontend/src/pages/main/main.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { Game } from "../../scenes/game/game";
+import socketService from "../../services/socket/socket";
+import { JoinRoom } from "../../components/joinRoom/joinRoom";
+import "./main.css";
+
+export const Main = () => {
+  const [isInRoom, setIsInRoom] = useState(false);
+
+  const connectSocket = async () => {
+    await socketService.connect("http://localhost:8000").catch((err) => {
+      console.log("Error: ", err);
+    });
+  };
+
+  useEffect(() => {
+    connectSocket();
+    console.log("useEffect run");
+  }, []);
+
+  return (
+    <div className="main-container">
+      
+      {!isInRoom && <JoinRoom setIsInRoom={setIsInRoom} />}
+      {isInRoom && <Game setIsInRoom={setIsInRoom} />}
+    </div>
+  );
+};
+
