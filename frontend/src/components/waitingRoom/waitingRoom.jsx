3aaf53ad26ba413dacd5fcbf008d3cc8cd401d16
Killian <paulic.killian@gmail.com>
2023-04-05 10:46:46 +0200

    waiting room style and update again the responsiveness

--- frontend/src/components/waitingRoom/waitingRoom.jsx
+++ frontend/src/components/waitingRoom/waitingRoom.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import gameService from "../../services/game/game";
 import "./waitingRoom.css";
+import { IoCopy } from "react-icons/io5";
 
 export const WaitingRoom = () => {
+  const copyRoomID = () => {
+    navigator.clipboard.writeText(gameService.roomId);
+  };
+
   return (
-    <div className="waiting-room-container">
-      {/* <h1>Waiting Room</h1> */}
-      <div className="waiting-room">
-        <h3 className="waiting-room-text">
-          Waiting for your opponent to join the room...
-        </h3>
-        <h3>Share this code with your friend: {gameService.roomId}</h3>
+    <div className="waiting-room">
+      <h3 className="waiting-room-text first-elem">
+        Waiting for your opponent to join the room...
+      </h3>
+      <h3 className="waiting-room-text">Share this code with your friend:</h3>
+      <div className="waiting-room-code">
+        {gameService.roomId}
+        <IoCopy className="copy-button" onClick={copyRoomID} />
       </div>
     </div>
   );
